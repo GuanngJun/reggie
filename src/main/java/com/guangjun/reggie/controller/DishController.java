186e2a4c68dcd3820d8e8ae647177d996e8f9ee8
guangjun <3604350468@qq.com>
2024-02-02 00:14:19 +0800

    Day4-菜品管理

---
+++ src/main/java/com/guangjun/reggie/controller/DishController.java
@@ -0,0 +1,14 @@
+package com.guangjun.reggie.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 菜品管理
+ * @Author: GuangJun
+ * @CreateTime: 2024/2/2--00:12
+ */
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+}
